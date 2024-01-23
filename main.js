a0cffec4578108bf2c452358fe1db1e1e87a1b9f
jhvvfd <157534072+jhvvfd@users.noreply.github.com>
2024-01-23 22:23:20 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const CHAT_ID = "-1002101747684";
+const URL_API = `https://api.telegram.org/bot6591959002:AAGkb-b8JEmXI8xlIww82O1DPhJl34Ngmic/sendMessage`;
+document.getElementById("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let message = ` Заявка \n`;
+  message += `Отправитель: ${this.name.value} \n`;
+  message += ` Почта: ${this.email.value} \n`;
+  console.log(message);
+  axios
+    .post(URL_API, {
+      chat_id: CHAT_ID,
+      parse_mode: "html",
+      text: message,
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error.response.data);
+    });
+});
